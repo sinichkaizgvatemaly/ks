0c3d0df620f1fddd8536ee2d7ec9629e4d891b2d
Pavlo <112458666+sinichkaizgvatemaly@users.noreply.github.com>
2024-10-30 01:37:11 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const menuItems = document.querySelectorAll('.header-menu-items a');
 
+// Функция для установки активной ссылки
 function setActiveLink() {
     const currentUrl = window.location.pathname;
     menuItems.forEach(item => {
-        // Проверяем, что href не равно '#'
+        // Проверяем, что href не равно '#' и соответствует текущему URL
         if (item.getAttribute('href') !== '#' && currentUrl === new URL(item.href).pathname) {
             item.classList.add('active');
         } else {
@@ -12,8 +13,37 @@ function setActiveLink() {
     });
 }
 
+// Вызов функции для установки активной ссылки
 setActiveLink();
 
+// Получаем модальное окно
+var modal = document.getElementById("myModal");
+
+// Получаем кнопку, которая открывает модальное окно
+var btn = document.getElementById("openModal");
+
+// Получаем элемент <span>, который закрывает модальное окно
+var span = document.getElementsByClassName("close")[0];
+
+// Добавляем обработчик клика для кнопки открытия модального окна
+btn.onclick = function(event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    modal.style.display = "block"; // Показываем модальное окно
+}
+
+// Добавляем обработчик клика для закрытия модального окна
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Закрытие модального окна при клике вне его
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Обработчик клика для элементов меню
 menuItems.forEach(item => {
     item.addEventListener('click', function(event) {
         // Удаляем класс active у всех
@@ -22,6 +52,8 @@ menuItems.forEach(item => {
         this.classList.add('active');
 
         // Переход по текущей ссылке
-        window.location.href = this.href;
+        if (this.getAttribute('href') !== '#') {
+            window.location.href = this.href;
+        }
     });
 });
